d1e930078d1268db208220db19ea7053a9e46b7c
Frlzz1e <nerdrummer@icloud.com>
2025-04-17 10:38:34 -0500

    recording

--- src/recording.c
+++ src/recording.c
@@ -5,18 +5,32 @@
 #include "list.h"
 #include "stdio.h"
 #include "math.h"
+#include "run.h"
+
 struct Sample **ListSample;
+struct Press **ListPress;
+
 void init()
 {
     ListSample = malloc(sizeof(struct Sample));
     ListSample[0] = SampleVoid;
 }
-void recordMusicPress()
+
+void recordMusicPress(enum KEYPAD key)
 {
     struct Sample *sample = malloc(sizeof(struct Sample));
-    sample->timeSinceFirstPressStart = pushSample(&ListSample, *sample);
+    struct Press *press = malloc(sizeof(struct Press));
+    int frequency = Frequencies[presetIndex][key];
+    int sampleIndex = pushSample(&ListSample, *sample);
+    press->RecordingIndex = sampleIndex;
+
 };
 
-void recordMusicRelease() {
+void recordMusicRelease()
+{
+    frequency = Frequencies[presetIndex][key];
+
+    struct Sample *sample = malloc(sizeof(struct Sample));
+
 
 };
