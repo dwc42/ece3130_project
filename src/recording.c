c34295ec3f3c80dd9c9f16a8e6e01b07f8255143
Frlzz1e <nerdrummer@icloud.com>
2025-04-17 10:02:14 -0500

    recording.c

---
+++ src/recording.c
@@ -0,0 +1,19 @@
+#include "stm32l4xx_hal.h"
+#include "events.h"
+#include "date.h"
+#include "lcd.h"
+#include "list.h"
+#include "stdio.h"
+#include "math.h"
+
+void recordMusicPress(){
+struct Sample *sample = malloc(struct Sample);
+
+};
+
+void recordMusicRelease(){
+
+
+
+
+};
